875e625fdd11b31791462d0e7371f4ecfea65bec
Replit user <>
2022-10-16 22:16:56 +0000

    new try

--- heap.c
+++ heap.c
@@ -34,7 +34,7 @@ void heap_pop(Heap* pq){
 
 Heap* createHeap(){
   Heap * newHeap = (Heap *)malloc(sizeof(Heap));
-  newHeap->heapArray = (heapElem *)malloc(3*sizeof(heapElem))
+  newHeap->heapArray = (heapElem *)malloc(3*sizeof(heapElem));
   newHeap->size = 0;
   newHeap->capac = 3;
   
